744902852b0f6337382b71473140712211d6a69d
Adam Towers <hello@adamtowers.design>
2018-02-15 04:11:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments) {
+  var theBeatlesPlay = [];
+  for (var i = 0; i < musicians.length; i++) {
+    theBeatlesPlay.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return theBeatlesPlay;
+}
+
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var johnLennonFacts = [];
+  let i = facts.length;
+  while (i < 0) {
+    --i;
+    johnLennonFacts.push(`${facts[i]}!!!`)
+  }
+  return johnLennonFacts;
+}
